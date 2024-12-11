7d395c8f12e930ad4d4349136cf06eeb735006f1
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-12-11 21:40:47 +0100

    [FRONT] Login {LoginSuccess,CreateToken}

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const authModel = require('../models/authModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 class authController {
     login = async (req, res) => {
         const { email, password } = req.body
@@ -15,6 +17,16 @@ class authController {
             if (user) {
                 const match = await bcrypt.compare(password, user.password)
                 if (match) {
+                    const obj = {
+                        id: user.id,
+                        name: user.name,
+                        category: user.category,
+                        role: user.role
+                    }
+                    const token = await jwt.sign(obj, process.env.secret, {
+                        expiresIn: process.env.exp_time
+                    })
+                    return res.status(200).json({ message: 'login success', token })
                     
                 } else {
                     return res.status(404).json({ message: 'invalid password' })
